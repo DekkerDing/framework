a0a73d9c6c6fd92082b9e7099bd678c678845e7b
Ding <Dekker160@163.com>
2023-01-03 12:03:24 +0800

    -- init -- subject [ Project structure depends on initialization ]

---
+++ security/src/main/java/io/github/DekkerDing/security/SecurityServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.DekkerDing.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+public class SecurityServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityServiceApplication.class, args);
+    }
+}
